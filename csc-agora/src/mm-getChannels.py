c86f8b138b4e0d06008abd9a65e44f453e8f6825
Willliam L. Anderson <band@acm.org>
2021-09-11 11:48:40 -0500

    added some comments

--- csc-agora/src/mm-getChannels.py
+++ csc-agora/src/mm-getChannels.py
@@ -7,7 +7,10 @@ import os
 import requests
 import json
 
+# Mattermost server access token required
 mm_token = os.environ['MM_PAT']
+
+# Collective Sense Commons specific Mattermost Team ID
 mm_teamid = 'yzebbrg9njfkdyt74a6kh69qke'
 
 def mm_channel_records(mm_teamid, mm_token):
@@ -24,22 +27,15 @@ def mm_channel_posts(channel_id, mm_token):
     )
     return r.json()
 
-def mm_user_record(user_id, mm_token):
-    r = requests.get(
-        f"https://chat.collectivesensecommons.org/api/v4/users/{user_id}",
-        headers={'Authorization': f'Bearer {mm_token}'}
-    )
-    return r.json()
-
 def main():
-    # cids = { 'display_name' : 'id' }
-    cids = {}
+    cids = {}     # cids = { channel_display_name : channel_id }
     try:
        data = mm_channel_records(mm_teamid, mm_token)
        for i in range(len(data)):
            cids[data[i].get('display_name')] = data[i].get('id')
        print(cids)
 
+       
     except Exception as e:
         traceback.print_exc(e)
 
