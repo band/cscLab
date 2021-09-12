f41b9e8996fefaf83ae700a71c96eeb7ff1da3cf
Willliam L. Anderson <band@acm.org>
2021-09-12 08:14:22 -0500

    testing caching and not

--- csc-agora/src/mm-getChannels.py
+++ csc-agora/src/mm-getChannels.py
@@ -2,15 +2,15 @@
 
 # Mattermost http API use: get channel names and ids
 
-import traceback
+import json
 import os
 import requests
-import json
+import traceback
 
 # Mattermost server access token required
 mm_token = os.environ['MM_PAT']
 
-# Collective Sense Commons specific Mattermost Team ID
+# Collective Sense Commons Mattermost Team 'Agora' ID
 mm_teamid = 'yzebbrg9njfkdyt74a6kh69qke'
 
 def mm_channel_records(mm_teamid, mm_token):
@@ -28,13 +28,19 @@ def mm_channel_posts(channel_id, mm_token):
     return r.json()
 
 def main():
+    ch_name = "OGM"
     cids = {}     # cids = { channel_display_name : channel_id }
     try:
        data = mm_channel_records(mm_teamid, mm_token)
        for i in range(len(data)):
            cids[data[i].get('display_name')] = data[i].get('id')
-       print(cids)
-
+       print(cids, '\n')
+       for i in range(len(data)):
+           if ch_name in data[i].get('display_name'):
+               print("*", ch_name,  data[i].get('id'), '\n')
+#       for cn in cids.keys():
+#           if ch_name in cn:
+#               print(ch_name, cids[cn], '\n')
        
     except Exception as e:
         traceback.print_exc(e)
