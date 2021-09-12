6d6fe5ac5c92adc8d198dc4b74d9768d9f839829
Willliam L. Anderson <band@acm.org>
2021-09-12 14:48:32 -0500

    working; returns messages from last channel match

--- csc-agora/src/mm-getMessages.py
+++ csc-agora/src/mm-getMessages.py
@@ -13,7 +13,6 @@ mm_token = os.environ['MM_PAT']
 
 # Collective Sense Commons Mattermost Team 'Agora' ID
 mm_teamid = 'yzebbrg9njfkdyt74a6kh69qke'
-channel_id = 'oq1nxzpozjygdke8szx47m3p8y'
 
 # set up argparse
 def init_argparse():
@@ -28,6 +27,20 @@ def mm_channel_records(mm_teamid, mm_token):
         headers={'Authorization': f'Bearer {mm_token}'}
     )
     return r.json()
+
+# get channel [id, display_name] list for a given MM team ID, and (partial) channel name;
+#    if more than 1 channel name matches, return the last match.
+def mm_channel_id(ch_name, mm_teamid, mm_token):
+    r = requests.get(
+        f"https://chat.collectivesensecommons.org/api/v4/teams/{mm_teamid}/channels",
+        headers={'Authorization': f'Bearer {mm_token}'}
+    )
+    data = r.json()
+    for i in range(len(data)):
+        if ch_name in data[i].get('display_name').casefold():
+            ch_id = [data[i].get('id'), data[i].get('display_name')]
+            print("* Found channel: ", ch_name,  data[i].get('id'), " ", data[i].get('display_name'), '\n')
+    return ch_id
 
 # get channel posts for a specified channel_id
 def mm_channel_posts(channel_id, mm_token):
@@ -53,17 +66,14 @@ def main():
     # ur = { 'user_id' : user_record } # cache user records
     ur = {}
     try:
-       data = mm_channel_records(mm_teamid, mm_token)
-       for i in range(len(data)):
-           if ch_name in data[i].get('display_name').casefold():
-               ch_id = data[i].get('id')
-               print("*", ch_name,  data[i].get('id'), '\n')
+        cid = mm_channel_id(ch_name, mm_teamid, mm_token)
+        print("** Usernames and messages from channel: ", cid[1], '\n')
         
-       data = mm_channel_posts(ch_id, mm_token)
-       for p in data["posts"].values():
-           if p["user_id"] not in ur.keys():
-               ur[p["user_id"]] = mm_user_record(p["user_id"], mm_token)
-           print(ur[p["user_id"]].get("username"), ": ", p["message"], '\n')
+        data = mm_channel_posts(cid[0], mm_token)
+        for p in data["posts"].values():
+            if p["user_id"] not in ur.keys():
+                ur[p["user_id"]] = mm_user_record(p["user_id"], mm_token)
+            print(ur[p["user_id"]].get("username"), ": ", p["message"], '\n')
 
     except Exception as e:
         traceback.print_exc(e)
