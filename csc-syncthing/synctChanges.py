5ce04ebe469b3e3a59e7f7c0539e0ae794d42165
William L. Anderson <band@acm.org>
2022-01-09 15:42:08 -0600

    (1) renamed source code file; (2) first use of TinyDB for a persistent changes store

---
+++ csc-syncthing/synctChanges.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Syncthing http REST API use
+#  record new pages (recent changes)
+
+import json
+import os
+import pprint
+import requests
+import traceback
+
+from datetime import datetime
+from tinydb import TinyDB, Query
+
+st_apikey = os.environ['SYNCTHING_API_KEY']
+
+def st_events(apikey):
+    r = requests.get(
+        f"http://localhost:8384/rest/events",
+        headers={'X-API-Key': apikey}
+    )
+    return r.json()
+
+pp=pprint.PrettyPrinter()
+db = TinyDB('./db.json')
+
+def main():
+    try:
+        events = st_events(st_apikey)
+        tpls = [tuple(x.get("data").get("filenames")) for x in events if x.get("type") == 'LocalIndexUpdated']
+        pp.pprint([x[0] for x in dict.fromkeys(tpls) if x[0].endswith('.md') and len(x) == 1])
+        for y in [x[0] for x in dict.fromkeys(tpls) if x[0].endswith('.md') and len(x) == 1]:
+            db.insert({'log_date':datetime.now().strftime("%Y-%m-%d"), 'filename': y})
+        
+    except Exception as e:
+        traceback.print_exc(e)
+
+if __name__ == "__main__":
+    exit(main())
+
+
