77a71acdf05ac691c5e2244b4e276c7e3e0f9892
Willliam L. Anderson <band@users.noreply.github.com>
2024-06-15 14:57:31 -0500

    baseline working something

---
+++ infoWorkbench/apaParse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+ apaParse: parse and print 'APAvar:' citations found in a specified Markdown file
+ (this is a very specific use case for Bill Anderson
+"""
+
+import re
+import logging
+
+# Configure basic logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+
+def find_apacitations(text):
+    year_pattern = r'\((?:19|20)\d{2}\)\.'
+    if (match := re.search(year_pattern, text)):
+        cite_year = match.group(0)
+        return cite_year
+    else:
+        return None
+
+def main():
+    # get filename from command-line input
+    text = ''
+    logging.info("citation years found: %s", find_apacitations(text))
+    return
+
+if __name__ == "__main__":
+    main()
