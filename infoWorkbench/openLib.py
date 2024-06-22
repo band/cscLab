fbcd64f176489654288819692b12bdf32cec8329
Willliam L. Anderson <band@users.noreply.github.com>
2024-06-22 16:24:54 -0500

    simplify logging output

--- infoWorkbench/openLib.py
+++ infoWorkbench/openLib.py
@@ -4,7 +4,7 @@ import requests
 import logging
 
 # Configure basic logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
 def query_openlibrary(title):
     """
